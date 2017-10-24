74fdfb37df09d3128aa912c9ca1a30ff0fb7d0b3
annabush092 <annabush092@gmail.com>
2017-10-24 18:03:03 -0400

    some refactoring to clean up index.js

---
+++ src/battle.js
@@ -0,0 +1,53 @@
+class Battle {
+
+  constructor() {
+  }
+
+  renderBattleDisplay() {
+    document.getElementById('game-screen').innerHTML = "<div id='battle-background'></div>"
+
+    document.getElementById('battle-background').innerHTML = `
+    <div id='left-display' class='pokemon-display'></div>
+    <div id='right-display' class='pokemon-display'></div>
+    <div id='menu'></div>`
+
+    document.getElementById('left-display').innerHTML = `
+    <div id='player-name' class='poke-name'></div>
+    <div id='player-stats' class='poke-stats'></div>
+    <div id='player-pic' class='poke-pic'></div>`
+
+    document.getElementById('right-display').innerHTML = `
+    <div id='opposing-pic' class='poke-pic'></div>
+    <div id='opposing-name' class='poke-name'></div>
+    <div id='opposing-stats' class='poke-stats'></div>`
+  }
+
+  renderBattle(player, opposing) {
+      document.getElementById('player-pic').innerHTML = `
+      <image id='player-pic-index' src="images/pokemon-pic-example.png">`
+
+      document.getElementById('opposing-pic').innerHTML = `
+      <image id='opposing-pic-index' src="images/pokemon-pic-example.png">`
+
+      document.getElementById('player-name').innerHTML = `
+      <div id='player-name-index'>Pokemon Name</div>`
+
+      document.getElementById('opposing-name').innerHTML = `
+      <div id='opposing-name-index'>Opposing Pokemon</div>`
+
+      document.getElementById('player-stats').innerHTML = `
+      <ul id='player-stats-index'>
+      <li>HP: </li>
+      <li>Speed: </li>
+      <li>Persistence: </li>
+      </ul>`
+
+      document.getElementById('opposing-stats').innerHTML = `
+      <ul id='opposing-stats-index'>
+      <li>HP: </li>
+      <li>Anger: </li>
+      <li>Melancholy: </li>
+      </ul>`
+  }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,56 +1,12 @@
 window.onload = function(){
 
-  function renderBattleDisplay() {
-    document.getElementById('game-screen').innerHTML = "<div id='battle-background'></div>"
+  fetch('')
+  .then(res => res.json())
+  .then(json => function(json){
+    console.log(json)
+  })
 
-    document.getElementById('battle-background').innerHTML = `
-    <div id='left-display' class='pokemon-display'></div>
-    <div id='right-display' class='pokemon-display'></div>
-    <div id='menu'></div>`
-
-    document.getElementById('left-display').innerHTML = `
-    <div id='player-name' class='poke-name'></div>
-    <div id='player-stats' class='poke-stats'></div>
-    <div id='player-pic' class='poke-pic'></div>`
-
-    document.getElementById('right-display').innerHTML = `
-    <div id='opposing-pic' class='poke-pic'></div>
-    <div id='opposing-name' class='poke-name'></div>
-    <div id='opposing-stats' class='poke-stats'></div>`
-  }
-
-  function renderBattle(player, opposing) {
-      document.getElementById('player-pic').innerHTML = `
-      <image id='player-pic-index' src="images/pokemon-pic-example.png">`
-
-      document.getElementById('opposing-pic').innerHTML = `
-      <image id='opposing-pic-index' src="images/pokemon-pic-example.png">`
-
-      document.getElementById('player-name').innerHTML = `
-      <div id='player-name-index'>Pokemon Name</div>`
-
-      document.getElementById('opposing-name').innerHTML = `
-      <div id='opposing-name-index'>Opposing Pokemon</div>`
-
-      document.getElementById('player-stats').innerHTML = `
-      <ul id='player-stats-index'>
-      <li>HP: </li>
-      <li>Speed: </li>
-      <li>Persistence: </li>
-      </ul>`
-
-      document.getElementById('opposing-stats').innerHTML = `
-      <ul id='opposing-stats-index'>
-      <li>HP: </li>
-      <li>Anger: </li>
-      <li>Melancholy: </li>
-      </ul>`
-  }
-
-
-
-
-
-  renderBattleDisplay()
-  renderBattle()
+  // battle = new Battle
+  // battle.renderBattleDisplay()
+  // battle.renderBattle()
 }
