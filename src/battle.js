7831f29434b0bf00158b3e7dd690ca9ab855420f
annabush092 <annabush092@gmail.com>
2017-10-27 13:45:32 -0400

    fixed cleaning up error

--- src/battle.js
+++ src/battle.js
@@ -7,18 +7,6 @@ class Battle {
     this.findPlayers();
   }
 
-  findPlayers() {
-    const pokeId = this.user.pokemon[0].pokemon_id
-    this.api = new Api
-    this.api.getPlayerPokemon(this, pokeId)
-  }
-
-  startGame(player, opposing) {
-    this.player = new Pokemon(player)
-    this.opposing = new Pokemon(opposing)
-    this.renderBattle()
-  }
-
   renderBattleDisplay() {
     document.getElementById('game-screen').innerHTML = "<div id='battle-background'></div>"
 
@@ -50,32 +38,38 @@ class Battle {
     `
   }
 
-  renderBattle() {
+  findPlayers() {
+    const pokeId = this.user.pokemon[0].pokemon_id
+    this.api = new Api
+    this.api.getPlayerPokemon(this, pokeId)
+  }
 
+  startGame(player, opposing) {
+    this.player = new Pokemon(player)
+    this.opposing = new Pokemon(opposing)
+    this.renderBattle()
+  }
+
+  renderBattle() {
     document.getElementById('player-pic').innerHTML =
     `<image id="player-pic" src="${this.player.back_default}">`
-
     document.getElementById('opposing-pic').innerHTML =
     `<image id="opposing-pic" src="${this.opposing.front_default}">`
 
     document.getElementById('player-name').innerHTML =
     `<div id="player-name">${this.player.name.toUpperCase()}</div>`
-
     document.getElementById('opposing-name').innerHTML =
     `<div id="opposing-name">${this.opposing.name.toUpperCase()}</div>`
 
     const playerStats = document.getElementById('player-stats')
     const playerStatList = document.createElement('ul')
-
     for(let i = 0; i < 6; i++){
       playerStatList.innerHTML += `<li id="${this.player.name}-${this.player.all_stats[i].stat_name}"> ${this.player.all_stats[i].stat_name}: ${this.player.all_stats[i].base_stat} </li>`
     }
     playerStats.appendChild(playerStatList)
 
-
     const opposingStats = document.getElementById('opposing-stats')
     const opposingStatList = document.createElement('ul')
-
     for(let i = 0; i < 6; i++){
       opposingStatList.innerHTML += `<li id="${this.opposing.name}-${this.opposing.all_stats[i].stat_name}"> ${this.opposing.all_stats[i].stat_name}: ${this.opposing.all_stats[i].base_stat} </li>`
     }
@@ -85,17 +79,12 @@ class Battle {
     for(let i = 0; i < this.player.all_moves.length; i++){
       moveMenu.innerHTML += `<option value="${this.player.all_moves[i].move_id}">${this.player.all_moves[i].move}</option>`
     }
-
-    console.log(this.player.id)
-
     menu.addEventListener("submit", function(e){
       e.preventDefault()
       const move = this.player.all_moves.find(x => x.move_id === parseInt(e.target[0].value))
       this.playerTurn(move)
     }.bind(this))
-
   }
-
 
   playerTurn(move) {
     if (move.stat_change.length > 0) {
@@ -109,31 +98,36 @@ class Battle {
     } else {
         this.updateStat({"hp": -2}, this.opposing)
     }
+    //move update stat to happen after the image flash
 
-    const flashCallBack = x => (
-      this.flashPlayer()
-    )
-
+    const flashCallBack = x => ( this.flashPlayer() )
     var intv = setInterval(flashCallBack, 100);
-
-    const endFlash = x => (
-      clearInterval(intv)
-    )
-
+    const endFlash = x => ( clearInterval(intv) )
     window.setTimeout(endFlash, 2000)
+    //make timeout slightly shorter than modal timeout so you have time to
+    //   reset image to default.
 
     const modalCallBack = x => (
-      this.addModal(`${this.player.name.toUpperCase()} uses ${move.move}! ${move.flavor_text}`, x => {
-        console.log("modal cb");
-        document.getElementById('game-modal').style.display = "none";
-        document.getElementById('modal-content').innerHTML = "";
-        this.checkWinner();
-        this.opposingTurn();
-      })
+      this.addModal(`${this.player.name.toUpperCase()} uses ${move.move}! ${move.flavor_text}`,
+        x => {
+          document.getElementById('game-modal').style.display = "none";
+          document.getElementById('modal-content').innerHTML = "";
+          this.checkWinner();
+          //make checkWinner return something to signal IF opposing faints, THEN
+          //   do not invoke opposingTurn
+          this.opposingTurn();
+        }
+      )
     )
     window.setTimeout(modalCallBack, 2000)
   }
 
+  updateStat(statHash, poke) {
+    let baseAttack = Math.floor(Math.random() * 10)
+    const stat = poke.all_stats.find(x => x.stat_name === Object.keys(statHash)[0])
+    stat.base_stat += (parseInt(Object.values(statHash)[0]) * baseAttack)
+    document.getElementById(`${poke.name}-${Object.keys(statHash)[0]}`).innerHTML = `${stat.stat_name}: ${stat.base_stat}`
+  }
 
   flashPlayer(){
     let playerPic = document.getElementById('player-pic')
@@ -144,9 +138,48 @@ class Battle {
     }
   }
 
+  addModal(message, cb){
+    document.getElementById('game-modal').style.display = "block"
+    let callBack = cb
+    let typeMessage = message
+    //do we need these definitions?
+    let i = 0
+    function typeWriter() {
+      if (i < typeMessage.length) {
+        document.getElementById('modal-content').innerHTML += typeMessage.charAt(i);
+        i++;
+        setTimeout(typeWriter, 50);
+      } else {
+        document.getElementById('modal-content').innerHTML += `<button class="close" id="modalButton"></button>`
+        document.getElementById('modalButton').addEventListener("click", callBack)
+      }
+    }
+    typeWriter()
+  }
+
+  checkWinner(){
+    // console.log("Player: ", this.player.all_stats[5].base_stat, "Opposing: ", this.opposing.all_stats[5].base_stat)
+    if(parseInt(this.player.all_stats[5].base_stat) <= 0){
+      this.bigFinale(this.player)
+      return
+    }else if (parseInt(this.opposing.all_stats[5].base_stat) <= 0) {
+      this.bigFinale(this.opposing)
+      return
+    }
+    //do we need return?
+    //the entire function should return if there is a winner or if the game should continue
+  }
+
+  bigFinale(poke){
+    this.addModal(`${poke.name.toUpperCase()} fainted!!!!!!`, function(){
+      document.getElementById('game-modal').style.display = "none";
+      this.app.getOrCreateUser();
+    }.bind(this))
+  }
 
   opposingTurn() {
     const move = this.opposing.all_moves[Math.floor(Math.random() * this.opposing.all_moves.length)]
+    //pull out into variables to make easier to read
     if (move.stat_change.length > 0) {
       for(const statHash of move.stat_change) {
         if(Object.values(statHash) > 0) {
@@ -158,26 +191,22 @@ class Battle {
     } else {
         this.updateStat({"hp": -2}, this.player)
     }
+    //can this if/else be its own method?
 
-    const flashCallBack = x => (
-      this.flashOpposing()
-    )
-
+    const flashCallBack = x => ( this.flashOpposing() )
     var intv = setInterval(flashCallBack, 100);
-
-    const endFlash = x => (
-      clearInterval(intv)
-    )
-
+    const endFlash = x => ( clearInterval(intv) )
     window.setTimeout(endFlash, 2000)
 
     const modalCallBack = x => (
-      this.addModal(`${this.opposing.name.toUpperCase()} uses ${move.move}! ${move.flavor_text}`, x => {
-        console.log("modal cb");
-        document.getElementById('game-modal').style.display = "none";
-        document.getElementById('modal-content').innerHTML = "";
-        this.checkWinner();
-      })
+      this.addModal(`${this.opposing.name.toUpperCase()} uses ${move.move}! ${move.flavor_text}`,
+        x => {
+          console.log("modal cb");
+          document.getElementById('game-modal').style.display = "none";
+          document.getElementById('modal-content').innerHTML = "";
+          this.checkWinner();
+        }
+      )
     )
     window.setTimeout(modalCallBack, 2000)
   }
@@ -192,52 +221,6 @@ class Battle {
       opposingPic.innerHTML= dull
     }
   }
-
-  checkWinner(){
-    console.log("Player: ", this.player.all_stats[5].base_stat, "Opposing: ", this.opposing.all_stats[5].base_stat)
-    if(parseInt(this.player.all_stats[5].base_stat) <= 0){
-      this.bigFinale(this.player)
-      return
-    }else if (parseInt(this.opposing.all_stats[5].base_stat) <= 0) {
-      this.bigFinale(this.opposing)
-      return
-    }
-  }
-
-  addModal(message, cb){
-    document.getElementById('game-modal').style.display = "block"
-
-    let callBack = cb
-    let typeMessage = message
-    let i = 0
-
-    function typeWriter() {
-      if (i < typeMessage.length) {
-        document.getElementById('modal-content').innerHTML += typeMessage.charAt(i);
-        i++;
-        setTimeout(typeWriter, 50);
-      } else {
-        document.getElementById('modal-content').innerHTML +=
-        `<button class="close" id="modalButton"></button>`
-
-        const button = document.getElementById('modalButton')
-        button.addEventListener("click", callBack)
-      }
-    }
-
-  updateStat(statHash, poke) {
-    let baseAttack = Math.floor(Math.random() * 10)
-    const stat = poke.all_stats.find(x => x.stat_name === Object.keys(statHash)[0])
-    stat.base_stat += (parseInt(Object.values(statHash)[0]) * baseAttack)
-    document.getElementById(`${poke.name}-${Object.keys(statHash)[0]}`).innerHTML = `${stat.stat_name}: ${stat.base_stat}`
-  }
-
-  bigFinale(poke){
-    this.addModal(`${poke.name.toUpperCase()} fainted!!!!!!`, function(){
-      document.getElementById('game-modal').style.display = "none";
-      this.app.getOrCreateUser();
-    }.bind(this))
-  }
-
+  //can this be the same method as flashPlayer?
 
 }
